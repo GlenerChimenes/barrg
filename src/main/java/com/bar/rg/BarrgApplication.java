000c7b725c1c216d99f7cba183eed06ce6fa09a6
GlenerChimenes <glener.chimenes@gmail.com>
2019-08-22 21:13:00 -0300

    Projeto criado

---
+++ src/main/java/com/bar/rg/BarrgApplication.java
@@ -0,0 +1,13 @@
+package com.bar.rg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarrgApplication.class, args);
+	}
+
+}
