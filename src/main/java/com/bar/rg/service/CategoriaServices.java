c4cd74649bf96e491b4697fbae3600f3db98611d
GlenerChimenes <glener.chimenes@gmail.com>
2019-09-17 23:05:13 -0300

    Listando todoas categoria com DTO

--- src/main/java/com/bar/rg/service/CategoriaServices.java
+++ src/main/java/com/bar/rg/service/CategoriaServices.java
@@ -1,5 +1,6 @@
 package com.bar.rg.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -44,4 +45,8 @@ public class CategoriaServices {
 		}
 		
 }
+
+	public List<Categoria> findAll() {
+		return repo.findAll();
+	}
 }
