7e667aa90c5b6a7ec051eb1c5ccfa4785b91e304
GlenerChimenes <glener.chimenes@gmail.com>
2019-09-25 23:24:02 -0300

    Validacao sintatica com Bean validat

--- src/main/java/com/bar/rg/service/CategoriaServices.java
+++ src/main/java/com/bar/rg/service/CategoriaServices.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.bar.rg.domain.Categoria;
+import com.bar.rg.dto.CategoriaDTO;
 import com.bar.rg.repository.CategoriaRepository;
 import com.bar.rg.service.exceptions.DataIntegrityException;
 import com.bar.rg.service.exceptions.ObjectNotFoundException;
@@ -56,6 +57,12 @@ public class CategoriaServices {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+		
+	}
+	
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 		
 	}
 	
